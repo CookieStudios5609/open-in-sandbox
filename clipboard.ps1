dc4e0e9b562b413d4aa2fc83dd3085793ca39b4e
Bake <78653311+CookieStudios5609@users.noreply.github.com>
2024-02-08 11:15:53 -0500

    Feat: Mount and open a path from clipboard

--- clipboard.ps1
+++ clipboard.ps1
@@ -1,15 +1,34 @@
-# A short mockup of how the clipboard action would be like.
 # Sandbox by default has clipboard redirection/passthrough
 # being able to launch this with Win + {1,2,3} when pinned to the taskbar is faster
-# Next is to make a context menu plugin for right clicking a file, consolidate and write in C3, 
-# add configurable settings/validate clipboard contains urls, etc.
+
+# context menu plugin/sharing target next
+# add Virustotal/hybridanalysis, checkshurturl, urlscan, and others
+
 $clip = Get-Clipboard
+
+$launchEdge = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe ${clip}"
+$launchExplore = "explorer C:\Users\WDAGUtilityAccount\Desktop\"
+
+if ($clip -like "*http*")
+    {$launchCommand = $launchEdge}
+elseif ($clip -like "*\*")  #  ^([A-Z]:\\)|(\\\\)
+    {
+    $launchCommand = $launchExplore
+    $mappedHost = "<MappedFolders>
+        <MappedFolder>
+            <HostFolder>$clip</HostFolder>
+        </MappedFolder>
+    </MappedFolders>"
+    write-output $mappedHost
+    }
+
 "
 <Configuration>
     <AudioInput>Disable</AudioInput>
     <ProtectedClient>Enable</ProtectedClient>
+    ${mappedHost}
     <LogonCommand>
-        <Command>C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe ${clip}</Command>
+        <Command>${launchCommand}</Command>
     </LogonCommand>
 </Configuration>
 " | Out-File -FilePath $env:TEMP\runSandbox.wsb
