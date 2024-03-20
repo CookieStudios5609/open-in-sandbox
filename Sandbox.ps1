3006b3894d6635599fc2717971380ef0fd163613
Bake <78653311+CookieStudios5609@users.noreply.github.com>
2024-03-20 12:26:47 -0400

    Feat: Copy clipboard.ps1 to launch from a folder as a param

---
+++ Sandbox.ps1
@@ -0,0 +1,19 @@
+﻿# args[0] doesn't work with powershell -File, as "%V" forces the path to be one argument.
+
+if ($args.Count) {
+"
+    <Configuration>
+        <AudioInput>Disable</AudioInput>
+        <ProtectedClient>Enable</ProtectedClient>
+        <MappedFolders>
+            <MappedFolder>
+                <HostFolder>${args}</HostFolder>
+            </MappedFolder>
+        </MappedFolders>
+        <LogonCommand>
+            <Command>explorer C:\Users\WDAGUtilityAccount\Desktop\</Command>
+        </LogonCommand>
+    </Configuration>
+" | Out-File -FilePath $env:TEMP\runSandbox.wsb
+Invoke-Item $env:TEMP\runSandbox.wsb
+}
